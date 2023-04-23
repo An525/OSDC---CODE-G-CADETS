2e17cb36da61a39365c22479e4f0525d69fe2aa7
Anshika <115554048+An525@users.noreply.github.com>
2023-04-23 23:39:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 include 'functions.php';
 $con = mysqli_connect("localhost","root","","shoppingcart");
 // Page is set to home (home.php) by default, so when the visitor visits that will be the page they see.
-$page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
+$page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'login';
 // Include and show the requested page
 include $page . '.php';
 ?>
