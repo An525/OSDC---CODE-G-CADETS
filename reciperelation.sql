36413eec27defdd788e92c7b19696125fa41e708
mannatchopra <131518926+mannatchopra@users.noreply.github.com>
2023-04-23 14:52:14 +0530

    Add files via upload

---
+++ reciperelation.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2023 at 10:26 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shoppingcart2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reciperelation`
+--
+
+CREATE TABLE `reciperelation` (
+  `id` int(11) NOT NULL,
+  `rid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reciperelation`
+--
+
+INSERT INTO `reciperelation` (`id`, `rid`) VALUES
+(1, 1),
+(6, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `reciperelation`
+--
+ALTER TABLE `reciperelation`
+  ADD PRIMARY KEY (`id`,`rid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
